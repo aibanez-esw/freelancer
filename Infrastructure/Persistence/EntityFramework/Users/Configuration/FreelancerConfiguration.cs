65576c4ece91e7a07ca032a24d96ccba718720d7
Antonio Ibáñez <aibanez@eshopworld.com>
2024-05-30 17:14:17 +0200

    feat: Add infrastructure layer with User and Freelancer configuration

---
+++ Infrastructure/Persistence/EntityFramework/Users/Configuration/FreelancerConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using Domain.Users.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Persistence.EntityFramework.Users.Configuration
+{
+    public class FreelancerConfiguration : UserConfiguration<Freelancer>
+    {
+        public override void Configure(EntityTypeBuilder<Freelancer> builder)
+        {
+            base.Configure(builder);
+            builder.ToTable("Freelancer").HasKey(f => f.Uuid);
+            builder.Property(c => c.Expertise).IsRequired().HasMaxLength(100);
+            builder.OwnsOne(c => c.ContactInformation);
+        }
+    }
+}
---
+++ Infrastructure/Persistence/EntityFramework/Users/Configuration/UserConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using Domain.Users.Entities;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Persistence.EntityFramework.Users.Configuration
+{
+    public class UserConfiguration<TEntity> : IEntityTypeConfiguration<TEntity>
+        where TEntity : User
+    {
+        private const string UuidColumnName = "uuid";
+        private const string UsernameColumnName = "username";
+        private const string PasswordColumnName = "password";
+        private const string EmailColumnName = "email";
+        private const string RoleColumnName = "role";
+
+        public virtual void Configure(EntityTypeBuilder<TEntity> builder)
+        {
+            builder.HasKey(u => u.Uuid);
+            builder.Property(u => u.Username).IsRequired();
+            builder.Property(u => u.Password).IsRequired();
+            builder.Property(u => u.Email).IsRequired();
+            builder.Property(u => u.Role).IsRequired();
+        }
+    }
+}
