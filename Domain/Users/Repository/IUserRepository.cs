ac57b3af237efd913e716339483be19d976adad6
Antonio Ibáñez <aibanez@eshopworld.com>
2024-05-30 12:14:52 +0200

    feat: Add IUserRepository

---
+++ Domain/Users/Repository/IUserRepository.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Users.Entities;
+
+namespace Domain.Users.Repository
+{
+    public interface IUserRepository
+    {
+        Task AddAsync(User user);
+        Task UpdateAsync(User user);
+        Task DeleteAsync(User user);
+        Task<User> GetAsync(Guid uuid);
+        Task<IEnumerable<User>> GetAllAsync();
+    }
+}
