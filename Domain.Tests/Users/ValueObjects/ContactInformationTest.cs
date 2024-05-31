1caa8f5d2dade1d66fa38b699d66816aac00aa62
Orleni Rodriguez <orodriguez@eshopworld.com>
2024-05-31 13:31:18 +0200

    Domain Test

---
+++ Domain.Tests/Users/ValueObjects/ContactInformationTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Users.ValueObjects;
+    
+namespace Domain.Tests.Users.ValueObjects
+{
+    public class ContactInformationTest
+    {
+        [Theory]
+        [MemberData(nameof(TestCorrectData))]
+        public async Task GivenCreateContactInformation_WhenCalledCorrect_ThenCreateNewContactInformation(string email, string phone, string address)
+        {
+            var contactInformation = ContactInformation.Create(email, phone, address);
+
+            Assert.Equal(email, contactInformation.Email);
+            Assert.Equal(phone, contactInformation.Phone);
+            Assert.Equal(address, contactInformation.Address);
+        }
+
+
+        public static IEnumerable<object[]> TestCorrectData
+        {
+            get
+            {
+                yield return new object[] { "email", "phone", "address" };
+            }
+        }
+
+
+
+    }
+}
---
+++ Domain.Tests/Users/ValueObjects/RoleTest.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Users.ValueObjects;
+
+namespace Domain.Tests.Users.ValueObjects
+{
+    public class RoleTest
+    {
+        [Theory]
+        [MemberData(nameof(TestCorrectData))]
+        public async Task GivenCreateRole_WhenCalledCorrect_ThenCreateNewRole( string value)
+        {
+            var role = Role.Create(value);
+
+            Assert.Equal(value, role.Value);
+        }
+
+        [Theory]
+        [MemberData(nameof(TestIncorrectData))]
+        public async Task GivenCreateRole_WhenCalledIncorrect_ThenCreateNewRole(string value)
+        {
+            Assert.Throws<Exception>(() => Role.Create(value));
+
+        }
+
+        public static IEnumerable<object[]> TestCorrectData
+        {
+            get
+            {
+                yield return new object[] { "Admin" };
+                yield return new object[] { "Client" };
+                yield return new object[] { "Freelancer" };
+            }
+        }
+
+        public static IEnumerable<object[]> TestIncorrectData
+        {
+            get
+            {
+                yield return new object[] { "Admintttt" };
+            }
+        }
+    }
+}
